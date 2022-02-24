31280b400db9bea1587b03a4c810de12b9c38860
DimaAmega <dima.amega1@gmail.com>
2022-02-24 23:26:52 +0300

    add tricommy curve

---
+++ two_clusters_kuramoto/limit_cycles_search/tricommy_curve.py
@@ -0,0 +1,36 @@
+import numpy as np
+import math as mt
+import pickle
+from numba import njit
+
+import os; cwd = os.path.dirname(os.path.abspath(__file__))
+package_path = os.path.normpath(os.path.join(cwd, "..", "..", ".."))
+import sys; sys.path.append(package_path)
+
+from calculation import optimizers
+
+def main():
+
+    @njit
+    def tricommy_curve(x):
+        return 4 / mt.pi * x - 0.305 * x ** 3
+    
+    def tricommy_curve_inv(x):
+        @njit
+        def wrap(y):
+            tc = tricommy_curve(y)
+            return (x - tc) ** 2
+        
+        return optimizers.newton(wrap, np.array([0.5]), eps=1e-6)
+    
+    h = 1e-2
+    a, b = 0, 1
+    intervals = int((b - a) / h)
+    x = np.linspace(a, b, intervals)
+    y = np.array([tricommy_curve_inv(x_i)[0] for x_i in x])
+    with open(f'tricommy_curve_inv_{a}_to_{b}_h_{h}.pickle', 'wb') as f:
+        pickle.dump(x, f)
+        pickle.dump(y, f)
+
+if __name__ == '__main__':
+    main()
