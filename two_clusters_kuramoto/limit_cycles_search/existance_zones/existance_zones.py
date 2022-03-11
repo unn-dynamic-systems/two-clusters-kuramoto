4930b8141c035b95edfb4910429f106114397688
DimaAmega <dima.amega1@gmail.com>
2022-03-11 23:54:35 +0300

    replace existance zones

--- two_clusters_kuramoto/limit_cycles_search/existance_zones/existance_zones.py
+++ two_clusters_kuramoto/limit_cycles_search/existance_zones/existance_zones.py
@@ -3,15 +3,12 @@ import math as mt
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pickle
-from numba import njit
 from scipy.interpolate import interp1d
 
 import os; cwd = os.path.dirname(os.path.abspath(__file__))
 package_path = os.path.normpath(os.path.join(cwd, "..", "..", ".."))
 import sys; sys.path.append(package_path)
 
-# from scipy.integrate import odeint
-from calculation import optimizers
 
 def get_interpolate_function_from_file(filepath):
     with open(filepath, 'rb') as f:
@@ -20,15 +17,16 @@ def get_interpolate_function_from_file(filepath):
         func = interp1d(x, y)
         return func
 
-def M(alpha, K, N):
-    tricommy_curve_inv = \
-    get_interpolate_function_from_file(f'tricommy_curve_inv_{0}_to_{1}_h_{0.01}.pickle')
-    def R(K, N, alpha):
-        return np.sqrt( ((N - 2 * K) ** 2) * (np.sin(alpha) ** 2) + (N ** 2) * (np.cos(alpha) ** 2) )
-    return N / R(K, N, alpha) / tricommy_curve_inv((N - 2 * K) / R(K, N, alpha) * np.absolute(np.sin(alpha))) ** 2
+cwd = os.path.dirname(os.path.abspath(__file__))
+tricommy_curve_inv = \
+    get_interpolate_function_from_file(f'{cwd}/tricommy_curve_inv_{0}_to_{1}_h_{0.01}.pickle')
+
+def M(Alpha, K, N):
+    def R(K, N, Alpha):
+        return np.sqrt( ((N - 2 * K) ** 2) * (np.sin(Alpha) ** 2) + (N ** 2) * (np.cos(Alpha) ** 2) )
+    return N / R(K, N, Alpha) / tricommy_curve_inv((N - 2 * K) / R(K, N, Alpha) * np.absolute(np.sin(Alpha))) ** 2
 
 def main():
-
     a_start, a_end = mt.pi / 6, 5 * mt.pi / 6
     a_h = 1e-3
     a_arr = np.linspace(a_start, a_end, int((a_end - a_start) / a_h))
@@ -54,7 +52,7 @@ def main():
     sns.lineplot(x=a_arr, y=m_arr,label=f"N={N}, K={K}", linestyle='--')
 
     plt.yscale('log')
-    plt.savefig(f'ex_zones.png')
+    plt.savefig(f'ex_zones2.png')
 
 if __name__ == '__main__':
     main()
