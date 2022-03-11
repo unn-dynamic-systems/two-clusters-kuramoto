c9342bf6c89c857dd708ebc5d2041b5c14ff79f2
DimaAmega <dima.amega1@gmail.com>
2022-03-11 23:56:19 +0300

    config change

--- two_clusters_kuramoto/limit_cycles_search/config.py
+++ two_clusters_kuramoto/limit_cycles_search/config.py
@@ -3,8 +3,7 @@ import math as mt
 
 class Config:
     data_storage = './data'
-    H_M = 1e-1
     N, K, Alpha = 5, 2, mt.pi / 2
     T0 = 80
     IC0 = np.array([0, 0.15])
-    Mass_end = 80
+    Mass_end = 10
