125a1f1a74c3dde89dde76186a8fc39977bedd1a
DimaAmega <dima.amega1@gmail.com>
2022-03-11 23:55:05 +0300

    rside fix

--- two_clusters_kuramoto/limit_cycles_search/rside.py
+++ two_clusters_kuramoto/limit_cycles_search/rside.py
@@ -7,6 +7,6 @@ class R_SIDES:
     def coupled_pendulums_rs(q, _, N, Mass, Alpha, K):
         X = np.empty(2)
         X[0] = q[1]
-        X[1] = 1 / Mass * ( (N - 2 * K) / N * np.sin(Alpha) - 1 / N * ((N - K) * np.sin(q[0] + Alpha) + K * np.sin(q[0] - Alpha)) - q[1])
+        X[1] = 1 / Mass * ( (N - 2 * K) / N * np.sin(Alpha) - \
+            1 / N * ((N - K) * np.sin(q[0] + Alpha) + K * np.sin(q[0] - Alpha)) - q[1])
         return X
-
