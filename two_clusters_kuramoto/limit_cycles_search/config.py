20cbabd45a2517cb4daf1522671dd228e626c051
DimaAmega <dima.amega1@gmail.com>
2022-03-17 15:12:30 +0300

    conf fix

--- two_clusters_kuramoto/limit_cycles_search/config.py
+++ two_clusters_kuramoto/limit_cycles_search/config.py
@@ -6,6 +6,11 @@ class Config:
     N = 5
     K = 2
     Alpha = mt.pi / 2
-    Mass_end = 5
+    Mass_start = 4.9
+    Mass_end = 90
     T0 = 80
     IC0 = np.array([0, 0.15])
+    h_m = 0.3
+    h_m_limit = 1e-2
+    h_a = 1e-2
+    h_a_limit = 1e-5
